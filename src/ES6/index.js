9e72655ddea8615a77b56ad3489edbbf2395753f
voidrizoma <milhadasdepapel@gmail.com>
2021-08-17 20:49:04 -0500

    ES6 folder

---
+++ src/ES6/index.js
@@ -0,0 +1,16 @@
+// Default params
+// before ES6
+function newName(name, age, country) {
+    var name = name || "karen"
+    var age = age || "32"
+    var country = country || "CDMX"
+    console.log(name, age, country)
+  
+  }
+  
+  // ES6
+  
+  function newName2(name = "Karen", age = "32", country = "CDMX") {
+    console.log(name, age, country)
+  }
+  
