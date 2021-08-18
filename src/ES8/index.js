4f9ceaa6dcf5bd19c9eb71005f42521069e64f0c
voidrizoma <milhadasdepapel@gmail.com>
2021-08-17 21:59:29 -0500

    object entries and values

---
+++ src/ES8/index.js
@@ -0,0 +1,15 @@
+//object entries .. returns a values from matrix
+
+const data = {
+    frontend: 'Karen',
+    backend: 'Alma',
+    designer: 'Diana'
+}
+
+const entries = Object.entries(data)
+console.log(entries)
+
+//Object values returns the values from an object to an array
+
+const values = Object.values(data)
+console.log(values)
