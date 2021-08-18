591d7327bbcdb461fcdb6a3184517c460c3e8743
voidrizoma <milhadasdepapel@gmail.com>
2021-08-17 20:59:12 -0500

    default params and template liretals

--- src/ES6/index.js
+++ src/ES6/index.js
@@ -9,8 +9,14 @@ function newName(name, age, country) {
   }
   
   // ES6
-  
-  function newName2(name = "Karen", age = "32", country = "CDMX") {
-    console.log(name, age, country)
-  }
-  
+function newName2(name = "Karen", age = "32", country = "CDMX") {
+  console.log(name, age, country)
+}
+
+//Concat
+
+let hello = 'hello'
+let phrase = 'phrase'
+
+let newPhrase = `${hello} ${phrase}`
+console.log(newPhrase)
