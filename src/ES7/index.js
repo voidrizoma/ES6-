0bddb6b407e626bb93fa3c72b2356294b1f3c9be
voidrizoma <milhadasdepapel@gmail.com>
2021-08-17 21:54:11 -0500

    ES7

---
+++ src/ES7/index.js
@@ -0,0 +1,14 @@
+//includes and exponential operators
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+if(number.includes(5)) {
+    console.log("Simòn, acà lo tenemos")
+} else {
+    console.log("no pues no")
+}
+
+let base = 4
+let exponent = 3
+let result = base** 3
+console.log(result)
+
+
